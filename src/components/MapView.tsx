40eb50bceec984ba0dfee3ae422432db1db14339
JorgeCY21 <jcondorios@unsa.edu.pe>
2025-10-04 12:32:41 -0500

    feat: add Leaflet and TypeScript types for improved map functionality; update MapView component to utilize sites data

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,6 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import sitesData from '../../public/data/tourist_sites.json'
 import 'leaflet/dist/leaflet.css'
+import sitesData from '../../public/data/tourist_sites.json'
 
 interface Props {
   region: string
@@ -12,16 +12,17 @@ function MapView({ region }: Props) {
   return (
     <div className="h-96 rounded-2xl overflow-hidden shadow">
       <MapContainer
-        center={[-9.19, -75.0152]} // Center of Peru
+        center={[-9.19, -75.0152] as [number, number]} // 👈 Type assertion para evitar error
         zoom={5}
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'} // 👈 Debe ser string
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
         {sites.map((site: any) => (
-          <Marker key={site.id} position={[site.lat, site.lng]}>
+          <Marker key={site.id} position={[site.lat, site.lng] as [number, number]}>
             <Popup>
               <strong>{site.name}</strong>
               <br />
